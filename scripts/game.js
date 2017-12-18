d8bb0eb8d35def8ba7ce5f393e7d5b6bf9244592
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2017-12-18 16:21:21 -0600

    Add files via upload

--- scripts/game.js
+++ scripts/game.js
@@ -2,19 +2,12 @@
 
 var loss = 0;
 var win = 0;
-var status = 0;
 var genNum = Math.floor(Math.random() * (102) + 19);
 var gemOne = Math.floor(Math.random() * 12) + 1;
 var gemTwo = Math.floor(Math.random() * 12) + 1;
 var gemThree = Math.floor(Math.random() * 12) + 1;
 var gemFour = Math.floor(Math.random() * 12) + 1;
-
-
-function calculate() {
-    status = gemOne + gemTwo;
-}
-
-
+var score = gemOne;
 $(document).ready(function () {
     $("#go").html(genNum);
     $("#g1").html(gemOne);
@@ -23,34 +16,32 @@ $(document).ready(function () {
     $("#g4").html(gemFour);
     $('#win').text(win);
     $('#loss').text(loss);
-    $('#status').text(status);
-    $("#g1").hide();
+    $('#score').text(score);
+    $("#g1").hide(); 
     $("#g2").hide();
     $("#g3").hide();
     $("#g4").hide();
-    $('#g1').click(function calculate () {
-     
-      
-    });
-    $('.gem-bx-2').click(function () {
-        math = math + blue;
-        config();
-    });
-    $('#yellow').click(function () {
-        math = math + yellow;
-        config();
-    });
-    $('#green').click(function () {
-        math = math + green;
-        config();
-    });    
-   
+
+// =================================
+
+    $("#red").click(function(){
+        $('#score').html(gemOne);
 });
+    });
+    $("#purple").click(function(){
+    $('#score').html(gemTwo);
+    });
+    $("#green").click(function(){
+        $('#score').html(gemThree);
+    });
+    $("#orange").click(function(){
+        $('#score').html(gemFour);
+    });
+
 console.log(genNum);
 console.log(gemOne);
 console.log(gemTwo);
 console.log(gemThree);
 console.log(gemFour);
-console.log(status);
-
-
+console.log(score);
+    
